d1cc2800646436f47b1008f5221ffe8ebad9c84e
Julian Stecklina <js@alien8.de>
2015-09-16 02:15:24 +0200

    Print branch statistics and abandonded remote branches.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
-
 #include <algorithm>
 #include <iostream>
+#include <chrono>
 
 #include <boost/program_options.hpp>
 #include <boost/format.hpp>
@@ -11,6 +11,17 @@
 
 namespace po = boost::program_options;
 using format = boost::format;
+
+static void erase_reference(std::vector<Git::Reference> &v, std::string const &name)
+{
+  auto found = std::find_if(v.begin(), v.end(), [&name] (Git::Reference const &r) { return name == r.branch_name(); });
+  if (found != v.end()) {
+    v.erase(found);
+
+    // Erase all other references that are equal.
+    erase_reference(v, name);
+  }
+}
 
 int main(int argc, char **argv)
 {
@@ -53,16 +64,21 @@ int main(int argc, char **argv)
       return commit_a->time() > commit_b->time();
     };
 
-    auto branches = Git::branches(repo, GIT_BRANCH_LOCAL);
-    std::sort(branches.begin(), branches.end(), ref_time_larger_sort);
+    auto remote_branches = Git::branches(repo, GIT_BRANCH_REMOTE);
+    auto local_branches = Git::branches(repo, GIT_BRANCH_LOCAL);
 
-    for (auto &ref : branches) {
+    std::sort(local_branches.begin(), local_branches.end(), ref_time_larger_sort);
+
+    time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+    for (auto &ref : local_branches) {
       auto upstream = ref.branch_upstream();
 
       if (not upstream) {
         std::cout << format("%1% tracks nothing.\n");
       } else {
-        auto mb = Git::mergebase(repo, ref, *upstream);
+        auto commit = Git::Commit::from_reference(repo, ref);
+        auto mb     = Git::mergebase(repo, ref, *upstream);
 
         if (not mb) {
           std::cout << format("%1% tracks %2% (no mergebase?)\n") % ref.branch_name() % upstream->branch_name();
@@ -70,11 +86,30 @@ int main(int argc, char **argv)
           size_t ahead  = revwalk(repo, *mb, ref).size();
           size_t behind = revwalk(repo, *mb, *upstream).size();
 
-          std::cout << format("%1% tracks %2% A%3%B%4%\n") % ref.branch_name() % upstream->branch_name() % ahead % behind; 
-          
+          time_t age = now - commit->time();
+
+          std::cout << format("%1% tracks %2% A%3%B%4%, last local commit %5%d ago\n") % ref.branch_name() % upstream->branch_name() % ahead % behind % (age / (3600 * 24));
+
+          erase_reference(remote_branches, upstream->branch_name());
         }
       }
     }
+
+    // remote_branches now contains all
+
+    std::sort(remote_branches.begin(), remote_branches.end(), ref_time_larger_sort);
+
+    for (auto &b : remote_branches) {
+      std::string branch_name = b.branch_name();
+
+      if ((branch_name.find("origin/") == std::string::npos) or
+          (branch_name.find("/HEAD") != std::string::npos)) {
+        continue;
+      }
+
+      std::cout << b.branch_name() << " is not pointed to by any local branch\n";
+    }
+
 
   } catch (Git::Error const &ge) {
     std::cerr << format("Git error %1%: %2%\n") % ge.error_code % ge.error->message;
